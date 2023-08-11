1730ff1e2db65175a177fbb15ea7d1c47628c16f
owen <owen.chosen@gmail.com>
2023-08-11 01:21:23 +0100

    Done

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener ('click', (event) => {
+  if (event.target.className === 'like-glyph' || event.target.className === 'like-glyph activated-heart') {
+    if (event.target.className === "like-glyph") {
+      mimicServerCall()
+      .then ( () => {
+        document.querySelector ("#modal").classList.add('hidden')
+        event.target.innerHTML = FULL_HEART
+        event.target.classList.add ('activated-heart')
+      })
+      .catch(() => {
+        document.querySelector ("#modal").classList.remove('hidden')
+        setTimeout (() => {
+          document.querySelector ("#modal").classList.add('hidden')
+        }, 3000)
+      })
+    }
+    else {
+      document.querySelector ("#modal").classList.add('hidden')
+      event.target.innerHTML = EMPTY_HEART
+      event.target.classList.remove ('activated-heart')
+    }
+
+  }
+})
+    
+
 
 
 
